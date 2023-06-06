e74d6b3c4250499a3b7b83054642fdf64b84ab5d
vipul.gupta <vipulgupta42all@gmail.com>
2023-06-06 21:27:03 +0530

    added home manager

--- flake.nix
+++ flake.nix
@@ -1,12 +1,25 @@
 {
-  inputs.nixpkgs.url = github:NixOS/nixpkgs;
-
-  outputs = { self, nixpkgs, ... }@args: {
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      specialArgs = { nixpkgs = nixpkgs; };
-      modules = [ ./configuration.nix ];
-    };
+  inputs ={
+    nixpkgs.url = github:NixOS/nixpkgs;
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+  outputs = args@{ self, nixpkgs, home-manager, ... }: {
+    nixosConfigurations = {
+        nixos = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          specialArgs = { nixpkgs = nixpkgs; };
+          modules = [
+            ./configuration.nix
+            home-manager.nixosModules.home-manager
+              {
+                home-manager.useGlobalPkgs = true;
+                home-manager.useUserPackages = true;
+                home-manager.users.vipul = import ./home.nix;
+              }
+          ];
+        };
+      };
     formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt;
   };
 }
---
+++ home.nix
@@ -0,0 +1,135 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "vipul";
+  home.homeDirectory = "/home/vipul";
+
+  # This value determines the Home Manager release that your configuration is
+  # compatible with. This helps avoid breakage when a new Home Manager release
+  # introduces backwards incompatible changes.
+  #
+  # You should not change this value, even if you update Home Manager. If you do
+  # want to update the value, then make sure to first check the Home Manager
+  # release notes.
+  home.stateVersion = "23.05"; # Please read the comment before changing.
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = with pkgs; [
+    nix-output-monitor # https://github.com/maralorn/nix-output-monitor
+    nix-info
+    cachix
+    lazygit # Better git UI
+    fzf
+    ripgrep # Better `grep`
+    nil # Nix language server
+  ];
+    # # Adds the 'hello' command to your environment. It prints a friendly
+    # # "Hello, world!" when run.
+    # pkgs.hello
+
+    # # It is sometimes useful to fine-tune packages, for example, by applying
+    # # overrides. You can do that directly here, just don't forget the
+    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
+    # # fonts?
+    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+
+    # # You can also create simple shell scripts directly inside your
+    # # configuration. For example, this adds a command 'my-hello' to your
+    # # environment:
+    # (pkgs.writeShellScriptBin "my-hello" ''
+    #   echo "Hello, ${config.home.username}!"
+    # '')
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    # # symlink to the Nix store copy.
+    # ".screenrc".source = dotfiles/screenrc;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  # You can also manage environment variables but you will have to manually
+  # source
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/vipulgupta/etc/profile.d/hm-session-vars.sh
+  #
+  # if you don't want to manage your shell through Home Manager.
+  home.sessionVariables = {
+    # EDITOR = "emacs";
+  };
+
+  # Let Home Manager install and manage itself.
+  # programs.home-manager.enable = true;
+  programs = {
+    # on macOS, you probably don't need this
+    home-manager = {
+      enable = true;
+    };
+
+    emacs = {                              
+      enable = true;
+      extraPackages = epkgs: [
+        epkgs.nix-mode
+        epkgs.magit
+      ];
+    };
+
+    bash = {
+      enable = true;
+      initExtra = ''
+        # Make Nix and home-manager installed things available in PATH.
+        export PATH=/run/current-system/sw/bin/:/nix/var/nix/profiles/default/bin:$HOME/.nix-profile/bin:/etc/profiles/per-user/$USER/bin:$PATH
+      '';
+    };
+
+    # For macOS's default shell.
+    zsh = {
+      enable = true;
+      autocd = true;
+      dotDir = ".config/zsh";
+      enableAutosuggestions = true;
+      enableCompletion = true;
+      shellAliases = {
+        l = "ls -la";
+      };
+      oh-my-zsh = {
+        enable = true;
+        theme = "robbyrussell";
+        plugins = [
+          "sudo"
+          "git"
+          "terraform"
+          "systemadmin"
+          "vi-mode"
+        ];
+      };
+      envExtra = ''
+        # Make Nix and home-manager installed things available in PATH.
+        export PATH=/run/current-system/sw/bin/:/nix/var/nix/profiles/default/bin:$HOME/.nix-profile/bin:/etc/profiles/per-user/$USER/bin:$PATH
+      '';
+    };
+
+    # https://haskell.flake.page/direnv
+    direnv = {
+      enable = true;
+      nix-direnv.enable = true;
+    };
+    starship.enable = true;
+    # Type `z <pat>` to cd to some directory
+    zoxide.enable = true;
+  };
+}
