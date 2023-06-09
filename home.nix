1995a5f879339421da986cd5a794bfe6d0d62f5e
vipul.gupta <vipulgupta42all@gmail.com>
2023-06-09 12:25:49 +0530

    fixed sudo, Path was wrong

--- home.nix
+++ home.nix
@@ -119,7 +119,7 @@
       };
       envExtra = ''
         # Make Nix and home-manager installed things available in PATH.
-        export PATH=/run/current-system/sw/bin/:/nix/var/nix/profiles/default/bin:$HOME/.nix-profile/bin:/etc/profiles/per-user/$USER/bin:$PATH
+        export PATH=/run/wrappers/bin/:/nix/var/nix/profiles/default/bin:$HOME/.nix-profile/bin:/etc/profiles/per-user/$USER/bin:$PATH
       '';
     };
 
